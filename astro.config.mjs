ba0f00e8a6bab5cd6572383eef8e2d42fe241372
Maruyama Takeshi <tac@sotai.co>
2024-06-01 14:39:46 +0900

    #2 Fix site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://sotai.co',
     integrations: [
         mdx(),
         sitemap(),
